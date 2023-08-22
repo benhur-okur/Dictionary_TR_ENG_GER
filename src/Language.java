ecf84b450e753016ddc5bbff8029e7aa30932daa
benhur-okur <117393881+benhur-okur@users.noreply.github.com>
2023-08-22 15:07:51 +0300

    first

---
+++ src/Language.java
@@ -0,0 +1,8 @@
+public class Language {
+    private String word;
+    private String wordMean;
+    private String wordDefinition;
+    private String usageInSentence;
+    private int correctGuess;
+
+}
